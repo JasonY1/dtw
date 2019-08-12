a1a5350b96bb3592420ee106f5100ff8d01129b0
jasony1 <jasonh.yi@gmail.com>
2019-08-12 20:41:22 +0000

    added verified params to admin side

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -82,7 +82,7 @@ Spree::Api::Config.configure do |config|
 end
 
 Spree.user_class = "Spree::LegacyUser"
-
+Spree::PermittedAttributes.user_attributes.push(:verified)
 # If you want to add a field to the whitelisted ransackable attributes,
 # just uncomment the following code and change it as you need.
 #
