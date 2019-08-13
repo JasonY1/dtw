d53f0be7dc0c7281ef6ea3c9e4eb52a5b0c600e6
jasony1 <jasonh.yi@gmail.com>
2019-08-13 23:04:15 +0000

    updated pages_controller

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 rake db:create
 rake db:setup
 
-rails g solidus_user_roles:install
-
-#seed
-rake solidus_user_roles:load_seeds
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_spree_user!
+  
   def Home
   end
 end
